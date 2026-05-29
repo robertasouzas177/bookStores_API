e82c7b0bc10a80233a62d8cff2d6d8763e3829eb
robertasouzas177 <robertasouzas177@gmail.com>
2026-05-29 20:36:24 -0300

    .

--- src/config/firebase.ts
+++ src/config/firebase.ts
@@ -1,13 +1,18 @@
 import admin from 'firebase-admin';
 
+const privateKey = process.env.FIREBASE_PRIVATE_KEY!
+  .replace(/\\n/g, '\n')
+  .replace(/^"|"$/g, ''); // remove aspas se houver
+
 admin.initializeApp({
   credential: admin.credential.cert({
     projectId: process.env.FIREBASE_PROJECT_ID!,
     clientEmail: process.env.FIREBASE_CLIENT_EMAIL!,
-    privateKey: process.env.FIREBASE_PRIVATE_KEY!.replace(/\\n/g, '\n'),
+    privateKey,
   })
 });
 
 const db = admin.firestore();
 
 export { db };
+
