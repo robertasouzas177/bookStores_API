111db78772f75132f00045b805c8620377c3425a
robertasouzas177 <robertasouzas177@gmail.com>
2026-05-29 20:03:02 -0300

    .

--- src/routes/users.routes.ts
+++ src/routes/users.routes.ts
@@ -1,7 +1,7 @@
 import { Router } from 'express';
 
 import usersController from '../controllers/users.controller';
-import authMiddleware from '../middlewares/auth.middleware';
+import authMiddleware from '../middlewares/auth.middleware'; 
 const router = Router();
 
 router.get('/', usersController.index);
