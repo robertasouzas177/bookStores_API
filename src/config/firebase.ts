5f951b6a6f77fb72f7ecaed8ef1f0453188d5c20
robertasouzas177 <robertasouzas177@gmail.com>
2026-05-29 20:28:33 -0300

    .

--- src/config/firebase.ts
+++ src/config/firebase.ts
@@ -1,8 +1,11 @@
 import admin from 'firebase-admin';
-import serviceAccount from './firebase-key.json';
 
 admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount as admin.ServiceAccount)
+  credential: admin.credential.cert({
+    projectId: process.env.FIREBASE_PROJECT_ID!,
+    clientEmail: process.env.FIREBASE_CLIENT_EMAIL!,
+    privateKey: process.env.FIREBASE_PRIVATE_KEY!.replace(/\\n/g, '\n'),
+  })
 });
 
 const db = admin.firestore();
