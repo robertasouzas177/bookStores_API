532897fd4b12b9f02f6813bfc1a1b8a401506de7
robertasouzas177 <robertasouzas177@gmail.com>
2026-05-29 20:43:00 -0300

    .

--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,7 @@
 import app from './app';
 
-const PORT = process.env.PORT || 3000;
+const PORT = Number(process.env.PORT) || 3000;
 
-app.listen(PORT, () => {
+app.listen(PORT, '0.0.0.0', () => {
   console.log(`Server running on port ${PORT}`);
 });
